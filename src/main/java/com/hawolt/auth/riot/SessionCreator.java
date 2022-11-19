1ecda87ea91e49bcacf29cbd7a05d539b140e7e6
hawolt <hawolton@gmail.com>
2022-11-19 15:42:29 +0100

    fixed auth

--- src/main/java/com/hawolt/auth/riot/SessionCreator.java
+++ src/main/java/com/hawolt/auth/riot/SessionCreator.java
@@ -1,7 +1,9 @@
 package com.hawolt.auth.riot;
 
+import com.hawolt.logger.Logger;
 import com.hawolt.misc.HttpClient;
 import com.hawolt.misc.StaticConfig;
+import kotlin.Pair;
 import okhttp3.*;
 
 import java.io.IOException;
@@ -20,15 +22,24 @@ public class SessionCreator {
     }
 
     public static String getCookie() throws IOException {
+        return getCookie(getCookie(null));
+    }
+
+    private static String getCookie(String __cf_bm) throws IOException {
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody post = RequestBody.create(StaticConfig.RIOT_POST, mediaType);
-        Request request = new Request.Builder()
-                .url("https://auth.riotgames.com/api/v1/authorization/")
+        Request.Builder builder = new Request.Builder()
+                .url("https://auth.riotgames.com/api/v1/authorization")
                 .addHeader("Content-Type", "application/json")
-                .post(post)
-                .build();
+                .addHeader("Accept", "application/json")
+                .addHeader("User-Agent", "RiotClient/18.0.0 (lol-store)")
+                .addHeader("Pragma", "no-cache")
+                .post(post);
+        if (__cf_bm != null) builder.addHeader("Cookie", __cf_bm);
+        Request request = builder.build();
         Call call = HttpClient.perform(request);
         try (Response response = call.execute()) {
+            if (__cf_bm == null) return build(response.headers("set-cookie"));
             if (response.code() == 200) {
                 return build(response.headers("set-cookie"));
             }
